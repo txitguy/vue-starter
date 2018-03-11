1c42680486490bd1e852b51a8987f0da0b48bf2f
Mark Roach <itwaguy@gmail.com>
2018-03-10 17:28:23 -0800

    Setup debugger

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import app from './app.vue'
 import router from './router'
 import 'normalize.css/normalize.css'
-import 'src/styles/themes/index.css'
 
 /* eslint-disable no-new */
 new Vue({
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as actions from './actions'
+//import * as actions from './actions'
 
 Vue.use(Vuex)
 
